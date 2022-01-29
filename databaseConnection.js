38b7f507ebd2a85543e7584fd12218c0752d1c4f
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-01-28 21:01:11 -0800

    initial commit

---
+++ databaseConnection.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+
+const is_heroku = process.env.IS_HEROKU || false;
+
+//mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
+const dbConfigHeroku = {
+	host: "us-cdbr-east-03.cleardb.com",
+	user: "b6fb24cb4c8c37",
+	password: "702be0f5",
+	database: "heroku_a79dfbc72e06757",
+	multipleStatements: false
+};
+
+const dbConfigLocal = {
+	host: "localhost",
+	user: "root",
+	password: "Password",
+	database: "database1",
+	multipleStatements: false
+};
+
+if (is_heroku) {
+	var database = mysql.createPool(dbConfigHeroku);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
